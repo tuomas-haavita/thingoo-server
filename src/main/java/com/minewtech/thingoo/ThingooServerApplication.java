72cfbe2adca21fb7211f1e6c015ce5f9c0775482
Tuomas Tammero <tuomas@three.consulting>
2020-02-23 10:31:33 +0200

    init

---
+++ src/main/java/com/minewtech/thingoo/ThingooServerApplication.java
@@ -0,0 +1,21 @@
+package com.minewtech.thingoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages ={"com.minewtech.thingoo.repository"})
+@EntityScan(basePackages ={ "com.minewtech.thingoo.model"})
+@EnableTransactionManagement
+@IntegrationComponentScan
+public class ThingooServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingooServerApplication.class, args);
+	}
+}
